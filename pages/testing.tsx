39f16164ee2f51735167d2cdd4424e678cdfaf62
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2022-10-05 20:16:39 +0200

    3%

--- pages/testing.tsx
+++ pages/testing.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import LoginForm from '../components/organism/RegisterForm';
 import Navbar from '../components/molecules/Navbar';
+import FilteringBar from '../components/molecules/FilteringBar';
 
 const Navbartest: NextPage = () => {
 const onChange = (value: string)=> console.log(value);
@@ -8,7 +9,7 @@ const onChange = (value: string)=> console.log(value);
   return (
     <div className="dark:text-white dark:bg-backgroundD bg-backgroundL w-[100%] m-0 h-screen">
       <Navbar value={"test"} searchbar={{onChange: onChange}}/>
-      <div></div>
+      <FilteringBar postTypeSelection={''}/>
     </div>
   )
 }
