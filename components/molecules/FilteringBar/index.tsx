75991708eca4635992c3020c0db0c05c184eab04
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2022-10-06 23:37:00 +0200

    20%

--- components/molecules/FilteringBar/index.tsx
+++ components/molecules/FilteringBar/index.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
+import FilterElement from "../../atoms/FilterElement";
+import Image from 'next/image';
+import nigditIcon from '../../../assets/testimage.svg'
 
 type PostFilters = {
     postTypeSelection: string,
@@ -6,10 +9,24 @@ type PostFilters = {
 
 export default function FilteringBar({postTypeSelection} : PostFilters)
 {
+    const clearHL = () => {
+
+    }
+
     return(
         <div className="w-[100%] flex justify-center flex-row items-center">
-            <div className="m-2 w-[30%] h-[4.5vh] min-h-[40px] max-h-[3rem] bg-foregroundL dark:bg-foregroundD border-black border-[1px] border-solid rounded-[5px] drop-shadow-minimalistic flex justify-center flex-row items-center">
-
+            <div className="m-2 p-0 w-[60%] ls:w-[40%] min-w-[320px] h-[4.5vh] min-h-[40px] max-h-[3rem] bg-foregroundL dark:bg-foregroundD border-black border-[1px] border-solid rounded-[5px] drop-shadow-minimalistic flex justify-between flex-row items-center">
+               <div className="flex justify-between flex-row items-center overflow-hidden h-[100%]">
+                <FilterElement name={"Hot"} clearHL={clearHL}/>
+                <FilterElement name={"New"} clearHL={clearHL}/>
+                <FilterElement name={"Top"} clearHL={clearHL}/>
+                <FilterElement name={"Pop"} clearHL={clearHL}/>
+               </div>
+                <div className="h-[100%] mr-1">
+                    <div className="rounded-[10px] hover:cursor-pointer h-[100%] w-[100%] flex justify-between flex-row items-center p-1">
+                        <Image src={nigditIcon} width={50} height={28} className="object-cover overflow-hidden w-[100%] h-[100%]"/>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from "next";
 import makpaj from '../assets/makpaj.svg'
+import Navbar from "../components/molecules/Navbar";
 import PostMedia from "../components/molecules/PostMedia";
 import PostText from "../components/molecules/PostText";
+
+const onChange = (value: string)=> console.log(value);
 
 const Home: NextPage = () => {
   const desc = `
@@ -16,11 +19,14 @@ const Home: NextPage = () => {
   `;
 
   return (
-    <div>
+    <div className="dark:text-white dark:bg-backgroundD bg-backgroundL w-[100%] m-0 h-screen">
+      <Navbar value={"test"} searchbar={{onChange: onChange}}/>
+      <div className="p-2">
       <PostText title="post" description={desc} author="user" date={new Date('2022-09-23')} source={{name: 'n/subnigdit', image:makpaj}} votes={1500} />
       <PostMedia title="gif post" media={{type: "gif", source: "https://c.tenor.com/hVm01utkmM8AAAAd/maciek-sze%C5%9Bcia%C5%84czyk-maciasek05.gif"}} author="makpaj" date={new Date('2000-09-23')} source={{name: 'n/subnigdit', image:makpaj}} votes={-1500} />
       <PostMedia title="gif post" media={{type: "video", source: "https://www.w3schools.com/html/mov_bbb.mp4"}} author="makpaj" date={new Date('2000-09-23')} source={{name: 'n/subnigdit', image:makpaj}} votes={-1500} />
 
+      </div>      
     </div>
   );
 };
