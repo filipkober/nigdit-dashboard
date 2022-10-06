d5a792910b73f1b21fd8ffc6826f4acd413fda26
Filip Kober <koberfilip2@gmail.com>
2022-10-06 19:59:02 +0200

    added navbar

--- pages/my-account.tsx
+++ pages/my-account.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next'
+import { useState } from 'react';
+import Navbar from '../components/molecules/Navbar';
 import MyAccountPanel from '../components/organism/MyAccountPanel';
 
 const MyAccount: NextPage = () => {
+  const [search, setSearch] = useState<string>("");
+  const changeSearch = (val: string) => {
+    setSearch(val);
+  }
   return (
     <>
-    {/* navbar */}
+    <Navbar searchbar={{value: search, onChange: changeSearch}}/>
     <MyAccountPanel/>
     </>
   )
