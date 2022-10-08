8ee51a87c07ef81597d1fe5fe785d58df2016440
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2022-10-08 18:15:00 +0200

    21%

--- components/molecules/FilteringBar/index.tsx
+++ components/molecules/FilteringBar/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useRef, useImperativeHandle, forwardRef } from "react";
 import FilterElement from "../../atoms/FilterElement";
 import Image from 'next/image';
 import nigditIcon from '../../../assets/testimage.svg'
@@ -6,21 +6,31 @@ import nigditIcon from '../../../assets/testimage.svg'
 type PostFilters = {
     postTypeSelection: string,
 }
+type RefHandler = {
+    childFunction: (val: string) => void
+}
 
 export default function FilteringBar({postTypeSelection} : PostFilters)
-{
-    const clearHL = () => {
-
+{    
+    const elementRef1 = useRef() as React.MutableRefObject<RefHandler>;
+    const elementRef2 = useRef() as React.MutableRefObject<RefHandler>;
+    const elementRef3 = useRef() as React.MutableRefObject<RefHandler>;
+    const elementRef4 = useRef() as React.MutableRefObject<RefHandler>;
+    const callClearHL = (val: string) => {
+        elementRef1.current.childFunction(val);
+        elementRef2.current.childFunction(val);
+        elementRef3.current.childFunction(val);
+        elementRef4.current.childFunction(val);
     }
 
     return(
         <div className="w-[100%] flex justify-center flex-row items-center">
             <div className="m-2 p-0 w-[60%] ls:w-[40%] min-w-[320px] h-[4.5vh] min-h-[40px] max-h-[3rem] bg-foregroundL dark:bg-foregroundD border-black border-[1px] border-solid rounded-[5px] drop-shadow-minimalistic flex justify-between flex-row items-center">
                <div className="flex justify-between flex-row items-center overflow-hidden h-[100%]">
-                <FilterElement name={"Hot"} clearHL={clearHL}/>
-                <FilterElement name={"New"} clearHL={clearHL}/>
-                <FilterElement name={"Top"} clearHL={clearHL}/>
-                <FilterElement name={"Pop"} clearHL={clearHL}/>
+                <FilterElement name={"Hot"} clearHL={callClearHL} ref={elementRef1}/>
+                <FilterElement name={"New"} clearHL={callClearHL} ref={elementRef2}/>
+                <FilterElement name={"Top"} clearHL={callClearHL} ref={elementRef3}/>
+                <FilterElement name={"Pop"} clearHL={callClearHL} ref={elementRef4}/>
                </div>
                 <div className="h-[100%] mr-1">
                     <div className="rounded-[10px] hover:cursor-pointer h-[100%] w-[100%] flex justify-between flex-row items-center p-1">
