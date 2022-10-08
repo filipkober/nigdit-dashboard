310791d55df510970cb1c5b1810dd3f7da294af6
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2022-10-08 21:15:41 +0200

    35%

--- components/atoms/FilterElement/index.tsx
+++ components/atoms/FilterElement/index.tsx
@@ -6,11 +6,29 @@ import nigditIcon from '../../../assets/testimage.svg'
 type Props = {
     name: string,
     clearHL: (val: string) => void,
+    initialVal?: boolean,
 }
 
-function FilterElement({name, clearHL}: Props, ref: ForwardedRef<any>)
+function FilterElement({name, clearHL, initialVal}: Props, ref: ForwardedRef<any>)
 {    
-    const [currentState, setCurrentState] = useState<boolean>(false);
+    const [currentState, setCurrentState] = useState<boolean>(initialVal || false);
+
+    useImperativeHandle(ref, () => ({
+        childFunction : (val: string): void => {
+            val == name ? setCurrentState(!currentState) : setCurrentState(false)
+        }
+    }))
+    return(
+        <div className="min-h-[2.4rem] h-[10vw] max-h-[100%] w-[20vw] max-w-[5.1rem] px-[1px] py-1">
+            <a onClick={() => {clearHL(name)}} className={currentState ? ("bg-foregroundL dark:bg-[#3c3c3c] rounded-[10px] hover:cursor-pointer h-[100%] w-[100%] flex justify-between flex-row items-center p-1") : ("bg-foregroundL dark:bg-foregroundD rounded-[10px] hover:cursor-pointer h-[100%] w-[100%] flex justify-between flex-row items-center p-1")}> {/*to boli, ale nie znam łatwiejszego rozwiązania*/}
+                <Image src={nigditIcon} width={26} height={26} className="object-cover overflow-hidden p-0 w-[100%] h-[100%] rounded-full"/>
+                <p className="shrink-1 text-[12px] ms:text-[14px] mm:text-[16px] ml:text-[18px] font-['Roboto'] dark:text-white">{name}</p>
+                <div></div>
+            </a>
+        </div>
+    )
+}
+export default forwardRef(FilterElement)
 
     // useImperativeHandle(ref, () => { 
     //     return { 
@@ -19,21 +37,3 @@ function FilterElement({name, clearHL}: Props, ref: ForwardedRef<any>)
     //         } 
     //     } 
     // },[]);
-
-    useImperativeHandle(ref, () => ({
-        childFunction : (val: string): void => {
-            val == name ? setCurrentState(!currentState) : setCurrentState(false)
-        }
-    }))
-    return(
-        <div className="min-h-[2.4rem] h-[10vw] max-h-[100%] w-[20vw] max-w-[5.4rem] p-1">
-            <a onClick={() => {clearHL(name)}} className={currentState ? ("bg-foregroundL dark:bg-[#3c3c3c] rounded-[10px] hover:cursor-pointer h-[100%] w-[100%] flex justify-between flex-row items-center p-1") : ("bg-foregroundL dark:bg-foregroundD rounded-[10px] hover:cursor-pointer h-[100%] w-[100%] flex justify-between flex-row items-center p-1")}> {/*to boli, ale nie znam łatwiejszego rozwiązania*/}
-                <Image src={nigditIcon} width={26} height={26} className="object-cover overflow-hidden p-0 w-[100%] h-[100%] rounded-full"/>
-                <p className="shrink-1 text-[12px] ts:text-[14px] tm:text-[16px] tl:text-[18px] font-['Roboto'] dark:text-white">{name}</p>
-                <div></div>
-            </a>
-        </div>
-    )
-}
-export default forwardRef(FilterElement)
-
