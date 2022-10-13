c56e94d1117830c65a9775b0f8e58d774504158b
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2022-10-13 20:24:30 +0200

    -naprawa błędów -DashboardFeed jako organizm -rosponsywność gotowa pod dodawanie postów

--- components/molecules/FilteringBar/index.tsx
+++ components/molecules/FilteringBar/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useImperativeHandle, forwardRef } from "react";
+import React, {useState, useRef, useImperativeHandle, forwardRef, useEffect } from "react";
 import FilterElement from "../../atoms/FilterElement";
 import Image from 'next/future/image';
 import bloodDrop from '../../../assets/blooddrop.svg';
@@ -7,11 +7,13 @@ import Blood from "../../atoms/Blood";
 
 type RefHandler = {
     childFunction: (val: string) => void
-    blood: (val: number) => void
+    blood: (val: number) => void    //🤫
+}
+type Props = {
 }
 
-export default function FilteringBar()
-{   
+export default function FilteringBar({}: Props)
+{     
     //const [parent] = useAutoAnimate<HTMLDivElement>({duration: 300, easing: 'ease-in-out'});
     const [collection,setCollection] = useState<string>("Everything");
     const clickCount = useRef<number>(0);    
@@ -21,7 +23,7 @@ export default function FilteringBar()
     {
         clickCount.current = clickCount.current+1;
         collection === "Everything" ? setCollection("Subscribed") : setCollection("Everything");
-        if (clickCount.current >= 16) bftbg.current.blood(Math.round(Math.random()*Math.min(clickCount.current-16,9)));        
+        if (clickCount.current >= 16) bftbg.current.blood(Math.round(Math.random()*Math.min(clickCount.current-16,9))); 
     }
 
     const elementRef1 = useRef() as React.MutableRefObject<RefHandler>; //nie mogłem odwołać wszystkiego do 1 elementu
@@ -36,30 +38,40 @@ export default function FilteringBar()
     }
 
     return(
-        <>
-        <div className="w-[100%] flex justify-center flex-row items-center">
-            <div className="m-2 p-0 ls:w-[50%] tl:w-[60%] tm:w-[70%] ts:w-[80%] ml:w-[90%] w-[100%] min-w-[320px] h-[4.5vh] min-h-[40px] max-h-[3rem] bg-foregroundL dark:bg-foregroundD border-black border-[1px] border-solid rounded-[5px] drop-shadow-minimalistic flex justify-between flex-row items-center">
-               <div className="flex justify-between flex-row items-center overflow-hidden h-[100%]">
-                <FilterElement name={"Hot"} clearHL={callClearHL} initialVal={true} ref={elementRef1}/>
-                <FilterElement name={"New"} clearHL={callClearHL} ref={elementRef2}/>
-                <FilterElement name={"Top"} clearHL={callClearHL} ref={elementRef3}/>
-                <FilterElement name={"Pop"} clearHL={callClearHL} ref={elementRef4}/>
-               </div>
-                <div className="h-[100%] ml:mr-1 pt-[3px] px-[2px]">
-                    <div className="flex justify-between flex-row items-center h-[80%]">  {/* specjalny przycisk: "użytkownik powinien wiedzieć, że może go kliknąć" */}
-                        <button onClick={swapCollection} className='hover:cursor-pointer shrink-1 text-[12px] ts:text-[14px] tm:text-[16px] tl:text-[18px] font-["Roboto"] dark:text-white active:translate-y-0.5 duration-[10ms] shrink-1 text-white text-center font-bold drop-shadow-buttonImp active:drop-shadow-buttonImpA border-black border-solid border-[1px] w-[100%] h-[100%] hover:bg-experimentB bg-experimentA px-2 rounded-[5px]'>{collection}</button>                                             
+        <>       
+        <div className="justify-start flex flex-col items-start w-[100%]">
+            <div className="flex justify-center flex-row items-center w-[100%]">
+                <div className="m-2 p-0 ls:w-[50vw] tl:w-[60vw] tm:w-[70vw] ts:w-[80vw] ml:w-[90vw] w-[100vw] min-w-[320px] h-[4.5vh] min-h-[40px] max-h-[3rem] bg-foregroundL dark:bg-foregroundD border-black border-[2px] border-solid rounded-[5px] drop-shadow-minimalistic flex justify-between flex-row items-center">
+                <div className="flex justify-between flex-row items-center overflow-hidden h-[100%]">
+                    <FilterElement name={"Hot"} clearHL={callClearHL} initialVal={true} ref={elementRef1}/>
+                    <FilterElement name={"New"} clearHL={callClearHL} ref={elementRef2}/>
+                    <FilterElement name={"Top"} clearHL={callClearHL} ref={elementRef3}/>
+                    <FilterElement name={"Pop"} clearHL={callClearHL} ref={elementRef4}/>
+                </div>
+                    <div className="h-[100%] ml:mr-1 pt-[3px] px-[2px]">
+                        <div className="flex justify-between flex-row items-center h-[80%]">  {/* specjalny przycisk: "użytkownik powinien wiedzieć, że może go kliknąć" */}
+                            <button onClick={swapCollection} className='hover:cursor-pointer shrink-1 text-[12px] ts:text-[14px] tm:text-[16px] tl:text-[18px] font-["Roboto"] dark:text-white active:translate-y-0.5 duration-[10ms] shrink-1 text-white text-center font-bold drop-shadow-buttonImp active:drop-shadow-buttonImpA border-black border-solid border-[1px] w-[100%] h-[100%] hover:bg-experimentB bg-experimentA px-2 rounded-[5px]'>{collection}</button>                                             
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>          
-        <Blood ref={bftbg}/>
-        {clickCount.current > 20 ? (        
-        <div className="flex translate-y-[-127px] animate-drip">
-            <div className="w-[100vw] h-[85px] bg-bloodDrip bg-repeat-x bg-contain cursor-no-drop"></div> 
-        </div> 
-        ) : (        
-        <div></div>
-        )}  
+            <Blood ref={bftbg}/>
+            {clickCount.current > 25 ? (        
+            <div className="fixed flex translate-y-[-70px] translate-x-[-22vw] animate-drip">
+                <div className="fixed w-[100vw] h-[85px] bg-bloodDrip bg-repeat-x bg-contain cursor-no-drop"></div> 
+            </div> 
+            ) : (        
+            <div></div>
+            )}  
+        </div>
         </>
     )
 }
+
+
+        // if (clickCount.current == 25)
+        // {
+        //     window.setInterval(() => {
+        //         bftbg.current.blood(4);
+        //     }, 700);  
+        // }
--- pages/testing.tsx
+++ pages/testing.tsx
@@ -1,17 +1,16 @@
 import type { NextPage } from 'next'
-import LoginForm from '../components/organism/RegisterForm';
 import Navbar from '../components/molecules/Navbar';
-import FilteringBar from '../components/molecules/FilteringBar';
+import DashboardFeed from '../components/organism/DashboardFeed';
 
-const Navbartest: NextPage = () => {
+const Testing: NextPage = () => {
 const onChange = (value: string)=> console.log(value);
 
   return (
     <div className="dark:text-white dark:bg-backgroundD bg-backgroundL w-[100%] m-0 h-screen">
       <Navbar value={"test"} searchbar={{onChange: onChange}}/>
-      <FilteringBar/>
+      <DashboardFeed/>
     </div>
   )
 }
 
-export default Navbartest
+export default Testing
