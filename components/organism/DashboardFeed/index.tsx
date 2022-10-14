30546ebc7e2633376f8107b0ac72a270b3015e3f
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2022-10-14 21:32:13 +0200

    niestety dodałem png

--- components/organism/DashboardFeed/index.tsx
+++ components/organism/DashboardFeed/index.tsx
@@ -1,28 +1,32 @@
 import React, {useState, useRef, useImperativeHandle, forwardRef, useEffect } from "react";
 import FilterElement from "../../atoms/FilterElement";
 import Image from 'next/future/image';
-import bloodDrop from '../../../assets/blooddrop.svg';
 import { useAutoAnimate } from '@formkit/auto-animate/react';
 import FilteringBar from "../../molecules/FilteringBar";
+import JoindeGroups from "../../atoms/JoinedGroups";
 
 export default function DashboardFeed() 
 {
-  //przenieś do nowego komponentu joinedBar
+  const [counter, setCounter] = useState<number>(0);
+  function clicked(cc: number)
+  {
+    setCounter(cc*2)    
+    console.log(counter)
+  }
 
   return (
     <>
       <div className="flex flex-row justify-between w-[100%]">
         <div className='tl:w-[22%] w-[0%] h-[6.3vh] min-h-[56px] max-h-[4rem] bg-[rgba(255,0,0,0)] tl:block hidden'></div>
-        <div className='tl:w-[56%] w-[100%] h-[6.3vh] min-h-[56px] max-h-[4rem] bg-[rgba(255,255,0,0)]'><FilteringBar/></div>
-        <div className='tl:w-[22%] w-[0%] h-[250px] bg-[rgba(255,0,255,0)] tl:block hidden'>
+        <div className='tl:w-[56%] w-[100%] h-[6.3vh] min-h-[56px] max-h-[4rem] bg-[rgba(255,255,0,0)]'><FilteringBar clicked={clicked}/></div>
+        <div className='tl:w-[22%] w-[0%] bg-[rgba(255,0,255,0)] tl:block hidden'>
           <div className=" w-[100%] h-[100%] flex flex-row justify-start tl:p-2">
-            <div className="w-[95%] max-w-[320px] h-[100%] bg-foregroundL dark:bg-foregroundD drop-shadow-midget rounded-[10px] border-black border-[2px] border-solid">
-              
-            </div>
+            <JoindeGroups/>
           </div>
         </div>
-      </div> 
+      </div>
+      <div style={{bottom: counter+"px"}} className={"fixed w-[100%] h-[100%] bg-ocean bg-repeat-x bg-cover cursor-no-drop translate-y-[90%]"}>
+      </div>
     </>
   )
 }
-
--- pages/testing.tsx
+++ pages/testing.tsx
@@ -6,10 +6,12 @@ const Testing: NextPage = () => {
 const onChange = (value: string)=> console.log(value);
 
   return (
-    <div className="dark:text-white dark:bg-backgroundD bg-backgroundL w-[100%] m-0 h-screen">
-      <Navbar value={"test"} searchbar={{onChange: onChange}}/>
-      <DashboardFeed/>
-    </div>
+    <>    
+      <div className="dark:text-white dark:bg-backgroundD bg-backgroundL w-[100%] m-0 h-screen">
+        <Navbar value={"test"} searchbar={{onChange: onChange}}/>
+        <DashboardFeed/>
+      </div>
+    </>
   )
 }
 
