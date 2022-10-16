7cb89cf0c4f7f579217d55469788eeaddfafd2c4
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2022-10-16 12:21:59 +0200

    fix

--- components/atoms/JoinedGroups/index.tsx
+++ components/atoms/JoinedGroups/index.tsx
@@ -10,14 +10,17 @@ type Props = {
 export default function JoindeGroups({initialVal}: Props)
 {    
     const [expanded, setExpanded] = useState<boolean>(initialVal || false);
-    const [test, setTest] = useState<number>(25);
-    const y = "h-["+test+"px]";
 
     return(
-        <div className="w-[95%] max-w-[320px] h-[320px] bg-foregroundL dark:bg-foregroundD drop-shadow-midget rounded-[10px] border-black border-[2px] border-solid">
-            <div className="w-[100%] flex flex-col justify-center items-center">
+        <div className="w-[95%] max-w-[320px] h-[40vh] bg-foregroundL dark:bg-foregroundD drop-shadow-midget rounded-[10px] border-black border-[2px] border-solid">
+            <div className="w-[100%] h-[100%] flex flex-col justify-center items-center">
                 <p className="m-[2px] shrink-1 text-[22px] ls:text-[24px] cs:text-[26px] cm:text-[28px] font-['Roboto'] dark:text-white">Subscriptions</p>
                 <hr className="h-[1px] border-solid border-[0px] w-[100%]" style={{backgroundImage: "linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.75), rgba(255, 255, 255, 0))"}}></hr>
+                <div className="w-[100%] h-[80%]">
+                    
+                </div>
+                <hr className="h-[1px] border-solid border-[0px] w-[100%]" style={{backgroundImage: "linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.75), rgba(255, 255, 255, 0))"}}></hr>
+                <p className="m-[2px] shrink-1 text-[18px] ls:text-[20px] cs:text-[22px] cm:text-[24px] font-['Roboto'] dark:text-white">Expand</p>
             </div>
         </div>
     )
