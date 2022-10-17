1679e96761f07c4ea98aee94dbbc337447e0cca3
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2022-10-17 20:30:23 +0200

    lista się teraz rozwija (●'◡'●)

--- components/molecules/JoinedGroups/index.tsx
+++ components/molecules/JoinedGroups/index.tsx
@@ -4,18 +4,51 @@ import nigditIcon from '../../../assets/testimage.svg'
 import GroupListElement from "../../atoms/GroupListElement";
 
 type Props = {
-    initialVal?: boolean,
+
+}
+
+type GroupListEl = {
+    name: string,
+    adress: string,
+    image: string,
 }
 
 
-export default function JoindeGroups({initialVal}: Props)
+export default function JoindeGroups({}: Props)
 {    
-    const [expanded, setExpanded] = useState<boolean>(initialVal || false);
+    const [expanded, setExpanded] = useState<boolean>(true);
+    const [h, seth] = useState<string>("28vh");
+    const [expandButtonText, setExpandButtonText] = useState<string>("Expand");
+
+    const subnigdits = [{name: "Fight44Reich",adress:"",image:nigditIcon},
+    {name: "Churchofsatan",adress:"",image:nigditIcon},
+    {name: "2110Biotech",adress:"",image:nigditIcon},
+    {name: "DankMemes",adress:"",image:nigditIcon},
+    {name: "TotalAnarchy",adress:"",image:nigditIcon},
+    {name: "Science",adress:"",image:nigditIcon},
+    {name: "12charactersmaxpls",adress:"",image:nigditIcon},
+    {name: "goatgore",adress:"https://kekma.live/",image:nigditIcon}]
+
+    function expand()
+    {
+        setExpanded(!expanded)
+        console.log("changed expansion of list of groups: "+expanded)
+        if(expanded == true)
+        {
+            seth((subnigdits.length*3).toString()+"vw")
+            setExpandButtonText("Collapse")            
+        }
+        else
+        {
+            seth("28vh")
+            setExpandButtonText("Expand")
+        }
+    }
 
     return(
-        <div className="object-fill w-[95%] max-w-[320px] h-[50vh] ls:h-[45vh] cs:h-[40vh] min-h-[200px] bg-foregroundL dark:bg-foregroundD drop-shadow-midget rounded-[10px] border-black border-[2px] border-solid">
-            <div className="w-[100%] h-[100%] flex flex-col object-fill">
-                <div className="flex flex-col justify-between items-center h-[15%] min-h-[40px]">
+        <div className={"object-fill w-[95%] max-w-[320px] min-h-[40vh] bg-foregroundL dark:bg-foregroundD drop-shadow-midget rounded-[10px] border-black border-[2px] border-solid"}>
+            <div className="w-[100%] flex flex-col object-fill justify-between">
+                <div className="flex flex-col justify-between items-center h-[6vh] min-h-[40px] max-h-[56px]">
                     <div className="w-[100%] h-[98%] flex flex-col justify-end items-start bg-joinedGroups bg-cover bg-left overflow-hidden object-cover rounded-[10px]">
                         <p className="mx-2 cs:mx-8 shrink-1 text-[15px] ls:text-[18px] cs:text-[20px] cm:text-[22px] font-['Roboto'] dark:text-white">Your Communities</p>
                     </div>
@@ -23,20 +56,19 @@ export default function JoindeGroups({initialVal}: Props)
                         <hr className="h-[1px] border-solid border-[0px] w-[100%] bg-black drop-shadow-lucifer"></hr>
                     </div>  
                 </div>
-                <div className="w-[100%] h-[70%] overflow-hidden bg-scroll bg-cover scrollbar-thin scrollbar-thumb-[#535353] scrollbar-track-[#2323232a] scrollbar-thumb-rounded-full scrollbar-track-rounded-full overflow-y-scroll "> 
-                    <GroupListElement num={1} name="Science" link="" image={nigditIcon}/>
-                    <GroupListElement num={2} name="2110Biotech" link="" image={nigditIcon}/>
-                    <GroupListElement num={3} name="Fight4Reich" link="" image={nigditIcon}/>
-                    <GroupListElement num={4} name="DankMemes" link="" image={nigditIcon}/>
-                    <GroupListElement num={5} name="MakpieGore" link="" image={nigditIcon}/>
-                    <GroupListElement num={6} name="TotalAnarchy" link="" image={nigditIcon}/>
+                <div style={{height: h}} className={"w-[100%] min-h-[28vh] overflow-hidden bg-scroll bg-cover scrollbar-thin scrollbar-thumb-[#535353] scrollbar-track-[#2323232a] scrollbar-thumb-rounded-full scrollbar-track-rounded-full overflow-y-scroll "}> 
+                    {subnigdits.map((x,index) =>{
+                        return(
+                            <GroupListElement key={index} num={index+1} name={x.name} link={x.adress} image={x.image}/>
+                        )
+                    })}
                 </div>
-                <div className="flex flex-col justify-between items-center h-[15%] min-h-[40px]">                    
+                <div className="flex flex-col justify-between items-center h-[6vh] min-h-[40px] max-h-[56px]">                    
                     <div className="w-[100%] h-[2%] min-h-[1px]">
                         <hr className="h-[1px] border-solid border-[0px] w-[100%] bg-black"></hr>
                     </div>                   
                     <div className="w-[100%] h-[98%] flex flex-col justify-center items-center p-2">
-                        <button className={`w-[100%] h-[100%] min-h-[30px] hover:cursor-pointer text-[18px] font-["Roboto"] text-white duration-[100ms] text-center font-bold hover:drop-shadow-midget border-black border-solid border-[1px] bg-experimentA hover:bg-experimentB rounded-full`}>Expand</button>
+                        <button onClick={expand} className={`w-[100%] h-[100%] min-h-[30px] hover:cursor-pointer text-[18px] font-["Roboto"] text-white duration-[100ms] text-center font-bold hover:drop-shadow-midget border-black border-solid border-[1px] bg-experimentA hover:bg-experimentB rounded-full`}>{expandButtonText}</button>
                     </div> 
                 </div>
             </div>
