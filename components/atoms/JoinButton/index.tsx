07ad73d68915af71c35d8596654f2deab8d50d72
Oliwier Kwiatkowski <oliwierkwiatkowski@protonmail.com>
2022-10-30 19:17:34 +0100

    basic subnigdit dashboard

--- components/atoms/JoinButton/index.tsx
+++ components/atoms/JoinButton/index.tsx
@@ -7,7 +7,11 @@ export function JoinButton() {
     <>
       <button
         onClick={() => setJoined(!joined)}
+<<<<<<< HEAD
         className="w-[8rem] justify-center py-3 font-semibold text-sm text-white rounded-full shadow-sm border border-white"
+=======
+        className="flex w-[8rem] justify-center py-3 font-semibold text-sm dark:bg-accentD text-white rounded-full shadow-sm"
+>>>>>>> aac0d54 (basic subnigdit dashboard)
       >
         {!joined ? 'Join' : 'Joined'}
       </button>
