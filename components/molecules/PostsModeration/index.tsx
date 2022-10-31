8361f84c0b2cdc1e020ce2fa2252bc480bfed035
Filip Kober <koberfilip2@gmail.com>
2022-10-31 19:57:25 +0100

    fixed responsive design

--- components/molecules/PostsModeration/index.tsx
+++ components/molecules/PostsModeration/index.tsx
@@ -8,7 +8,7 @@ type PostsModerationProps = {
 
 export default function PostsModeration({className}: PostsModerationProps){
     type postTypeType = "text" | "image" | "video";
-    const [random, setRandom] = useState<number>(Math.floor(Math.random() * 3));
+    const [random, setRandom] = useState<number>(2);
 
     let postType: postTypeType;
     if(random === 0){
@@ -24,7 +24,7 @@ export default function PostsModeration({className}: PostsModerationProps){
 
     let postContent: JSX.Element = <></>;
     if(postType === "text"){
-        postContent = <p className="col-start-2 row-start-3 col-span-full ts:text-3xl">{"This is a post content"}</p>
+        postContent = <p className="col-start-2 row-start-3 col-span-full row-span-3 ts:text-3xl overflow-scroll border-black border-2 p-1 dark:border-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ipsa doloribus quidem est, eligendi architecto sapiente tempora. Voluptates quibusdam exercitationem unde ut repellat eius ea sapiente eligendi, at, iure nisi. Eius expedita deserunt, odio facilis, doloribus dolorum eum quisquam eos neque ipsam placeat fugiat mollitia. Ipsam corporis corrupti tempore ex a, accusantium assumenda laudantium ipsum vitae, excepturi deserunt earum! Rerum maiores deleniti sequi praesentium quasi quibusdam ipsam velit dolore, ullam, porro quisquam in voluptate possimus. Laboriosam pariatur dolorem nihil enim, vitae tenetur impedit tempore quibusdam dolor velit quis odit, suscipit omnis, incidunt nobis natus mollitia saepe explicabo aperiam iusto. Rem libero ullam sequi fugit maiores mollitia, nam inventore distinctio odio similique aspernatur, quibusdam in nihil veniam sed ratione dignissimos, recusandae quidem qui eveniet. Aperiam incidunt nobis laboriosam porro enim? Assumenda quam aliquid, odit quos fugiat officiis magnam repellendus, possimus aliquam magni esse dolor culpa iusto porro placeat, quisquam dicta suscipit.</p>
     } else if(postType === "image"){
         postContent = <Image src={"https://source.unsplash.com/random/800x1500"} alt="makpaj" width={800} height={200} className="col-start-2 row-start-3 col-span-full row-span-3 max-h-[40vh] object-cover w-auto" loader={() => imgSrc} unoptimized/>
     } else if(postType === "video"){
@@ -41,7 +41,7 @@ export default function PostsModeration({className}: PostsModerationProps){
             <p className="col-start-1 row-start-6 ts:text-3xl">Actions:</p>
             <Button className="col-start-2 row-start-6 ts:text-3xl" variant="button" content="Ban user" />
             <Button className="col-start-3 row-start-6 ts:text-3xl" variant="button" content="Delete post" />
-            <Button className="col-start-4 row-start-6 ts:text-3xl" variant="button" content="Skip" />
+            <Button className="col-start-4 row-start-6 ts:text-3xl" variant="button" content="Skip" onClick={() => {random === 2 ? setRandom(0) : setRandom(random + 1)}} />
         </div>
     )
 }
