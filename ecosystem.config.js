47b20abce5690b81b2fab799c60801e1856363a9
Filip Kober <koberfilip2@gmail.com>
2022-11-01 19:38:17 +0100

    added ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    script: 'npm start',
+  }],
+
+  deploy : {
+    production : {
+      key: 'nigdit_key.pem',
+      user : 'azureuser',
+      host : '51.142.107.198',
+      ref  : 'origin/master',
+      repo : 'git@github.com:filipkober/nigdit-dashboard.git',
+      path : '/home/azureuser',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
