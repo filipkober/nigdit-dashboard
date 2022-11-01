5697ccd88d644ac3a0ada801da6cb7c21d8bf0d2
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2022-11-01 12:38:14 +0100

    -tailwind config (new button colors) -visual improvement of some old atoms, -added subnigdit creator

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,6 +39,7 @@ module.exports = {
       },
       colors:{
         backgroundD: '#1E1E1E',
+        highlightD: '#252525',
         foregroundD: '#2B2B2B',
         backgroundL: '#D9A7DA',
         foregroundL: '#FF00FF',
@@ -52,6 +53,8 @@ module.exports = {
         cancelH: '#8F0000',
         apply: '#007000',
         applyH: '#008F00',
+        delete: '#350000',
+        deleteH: '#5A0000',
       },
       animation: {
         'droplet1': 'drop 1.4s cubic-bezier(.57,0,.75,.07)',
