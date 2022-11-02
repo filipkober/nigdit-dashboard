14fc788218382f44831a8954fe1b487787a588f2
Oliwier Kwiatkowski <oliwierkwiatkowski@protonmail.com>
2022-11-02 19:05:11 +0100

    stage 1 select

--- components/organism/SubnigditDashboard/index.tsx
+++ components/organism/SubnigditDashboard/index.tsx
@@ -6,6 +6,8 @@ import PostText from '../../molecules/PostText';
 import SubnigditRules from '../../molecules/SubnigditRules';
 import DashboardFeed from '../DashboardFeed';
 import makpaj from '../../../assets/makpaj.svg';
+import TabSelector from '../../molecules/TabSelector';
+import { useState } from 'react';
 
 const desc = `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -19,59 +21,76 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 `; //temp
 
 export default function SubnigditDashboard() {
+  const [selected, setSelected] = useState<number>(0);
+
   return (
     <>
-      <div className='h-full'>
+      <div className="h-full">
         <div className="">
           <DashboardHeader />
         </div>
-        <div>
-          <FilteringBar
-            clicked={function (cc: number): void {
-              throw new Error('Function not implemented.');
-            }}
+
+        {/*Mobile View*/}
+        <div className="ls:hidden inline">
+          <TabSelector
+            selected={selected}
+            setSelected={setSelected}
+            tabs={['Posts', 'Other']}
           />
         </div>
-        <div className="flex flex-row justify-around ">
-          <div className="w-[20vw]">
-            <SubnigditRules />
-          </div>
-          <div className="ls:w-[50vw] tl:w-[60vw] tm:w-[70vw] ts:w-[80vw] ml:w-[90vw] w-[100vw] min-w-[320px] flex flex-col">
-            {/* tu będzie map */}
-            <PostMedia
-              title="gif post"
-              media={{
-                type: 'gif',
-                source:
-                  'https://c.tenor.com/hVm01utkmM8AAAAd/maciek-sze%C5%9Bcia%C5%84czyk-maciasek05.gif',
-              }}
-              author="makpaj"
-              date={new Date('2000-09-23')}
-              source={{ name: 'n/subnigdit', image: makpaj }}
-              votes={-1500}
-            />
-            <PostText
-              title="post"
-              description={desc}
-              author="user"
-              date={new Date('2022-09-23')}
-              source={{ name: 'n/subnigdit', image: makpaj }}
-              votes={1500}
-            />
-            <PostMedia
-              title="gif post"
-              media={{
-                type: 'video',
-                source: 'https://www.w3schools.com/html/mov_bbb.mp4',
-              }}
-              author="makpaj"
-              date={new Date('2000-09-23')}
-              source={{ name: 'n/subnigdit', image: makpaj }}
-              votes={-1500}
-            />
-          </div>
-          <div className="w-[20vw]">
-            <SubnigditRules />
+
+        {/*Desktop View*/}
+        <div className="hidden ls:inline">
+          <div className="flex flex-row justify-around">
+            <div className="w-[20vw] my-2">
+              <SubnigditRules />
+            </div>
+            <div className="ls:w-[50vw] tl:w-[60vw] tm:w-[70vw] ts:w-[80vw] ml:w-[90vw] w-[100vw] min-w-[320px] flex flex-col">
+              <div className="mb-[1vh]">
+                <FilteringBar
+                  clicked={function (cc: number): void {
+                    throw new Error('Function not implemented.');
+                  }}
+                />
+              </div>
+              {/* tu będzie map */}
+              <div className="px-2">
+                <PostMedia
+                  title="gif post"
+                  media={{
+                    type: 'gif',
+                    source:
+                      'https://c.tenor.com/hVm01utkmM8AAAAd/maciek-sze%C5%9Bcia%C5%84czyk-maciasek05.gif',
+                  }}
+                  author="makpaj"
+                  date={new Date('2000-09-23')}
+                  source={{ name: 'n/subnigdit', image: makpaj }}
+                  votes={-1500}
+                />
+                <PostText
+                  title="post"
+                  description={desc}
+                  author="user"
+                  date={new Date('2022-09-23')}
+                  source={{ name: 'n/subnigdit', image: makpaj }}
+                  votes={1500}
+                />
+                <PostMedia
+                  title="gif post"
+                  media={{
+                    type: 'video',
+                    source: 'https://www.w3schools.com/html/mov_bbb.mp4',
+                  }}
+                  author="makpaj"
+                  date={new Date('2000-09-23')}
+                  source={{ name: 'n/subnigdit', image: makpaj }}
+                  votes={-1500}
+                />
+              </div>
+            </div>
+            <div className="w-[20vw] my-2">
+              <SubnigditRules />
+            </div>
           </div>
         </div>
       </div>
