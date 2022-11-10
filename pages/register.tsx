3a3c1b29ad0131fcbf254a27bf0468a103d7dee7
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2022-11-10 18:45:46 +0100

    verify1

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,11 +1,25 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react';
+import EmailVerification from '../components/organism/EmailVerification';
 import RegisterForm from '../components/organism/RegisterForm';
 
 const Register: NextPage = () => {
 
+  const [verify,setVerify] = useState<boolean>(false);
+
+  function verChange(val: boolean)
+  {
+    setVerify(val)
+  }  
+
   return (    
-    <div className="dark:text-white dark:bg-backgroundD bg-backgroundL w-[100%] m-0 h-screen">      
-      <RegisterForm/>
+    
+    <div className="dark:text-white dark:bg-backgroundD bg-backgroundL w-[100%] p-0 m-0 h-screen">
+      {verify == true? (
+        <EmailVerification verChange={verChange} email={"Niggers@must.die"}/>
+      ):(
+        <RegisterForm verChange={verChange}/>
+      )}
     </div>
     
   )
