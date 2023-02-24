0cab04276aa06b81bd3091c197f01b3c9ea10159
Filip Kober <koberfilip2@gmail.com>
2023-02-24 19:34:53 +0100

    Added web request snippet

---
+++ models/Reply.ts
@@ -0,0 +1,18 @@
+type Reply = {
+    id: number,
+    content: string,
+    votes: number,
+    created_at: string,
+}
+
+type StrapiReply = {
+    id: number,
+    attributes: {
+        content: string,
+        votes: number,
+        created_at: string,
+    },
+}
+
+export default Reply
+export type { StrapiReply };
--- pages/ballin.tsx
+++ pages/ballin.tsx
@@ -1,5 +1,9 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
+import Reply from '../components/atoms/Reply'
+import { StrapiReply } from '../models/Reply'
+import StrapiResponse, { strapiResponseToData } from '../models/StrapiResponse'
 import { incrementCounter } from '../store/userSlice'
 
 const Ballin: NextPage = () => {
@@ -10,11 +14,24 @@ const Ballin: NextPage = () => {
     dispatch(incrementCounter())
   }
 
+  const [replies, setReplies] = useState<StrapiReply[]>([])
 
+  useEffect(() => {
+    fetch('http://localhost:1338/api/replies').then((res) => {
+      res.json().then((data: StrapiResponse<StrapiReply[]>) => {
+        setReplies(strapiResponseToData(data))
+      })
+    })
+  }, [])
+
+  console.log(replies)
 
   return (
     <div>
       <button onClick={onClick}>Klik klik</button>
+      {replies.map((reply) => (
+        <Reply key={reply.id} id={reply.id} votes={reply.attributes.votes} pfp={''} nick={''} content={reply.attributes.content} responseTo={0}  />
+      ))}
     </div>
   )
 }
