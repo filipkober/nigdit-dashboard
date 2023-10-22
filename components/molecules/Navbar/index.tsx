ec835542a66c77db95165e6d89a69a577acff4b9
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2023-10-22 21:21:49 +0200

    added mobile burger icon and slots for other icons to the navbar

--- components/molecules/Navbar/index.tsx
+++ components/molecules/Navbar/index.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { InputHTMLAttributes, useCallback, useEffect, useState } from 'react';
+import { BiMessageAdd } from 'react-icons/Bi';
+import { RxHamburgerMenu } from 'react-icons/Rx';
 import { useSelector } from 'react-redux';
 import emptypfp from '../../../assets/emptypfp.jpg';
 import nigditIcon from '../../../assets/testimage.svg';
@@ -10,7 +12,6 @@ import { SubnigditSearchResult } from '../../../models/Subnigdit';
 import { UserState } from '../../../store/userSlice';
 import SubnigditService from '../../../util/requests/SubnigditService';
 import SubnigditSearch from '../SubnigditSearch';
-
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
     user?:
     {
@@ -60,15 +61,19 @@ export default function Navbar()
 
     return(
         <div className="pointer-events-none flex flex-row justify-between h-[5.5vh] min-h-[52px] max-h-[3.2rem] w-[100%] overflow-hidden bg-foregroundL dark:bg-foregroundD border-black border-b-2 border-solid sticky z-[40] top-0 left-0">
-            <Link href="/" className='pointer-events-auto min-w-[2.4rem] w-[2.4rem] ml:w-[7.4rem] tl:max-w-[13rem] h-[100%] flex flex-row my-2 mx-2'>
-                <div className='shrink-0'>
-                    <Image draggable="false" src={nigditIcon} width={36} height={36} className="select-none hover:cursor-pointer object-cover overflow-hidden p-0 w-[2.4rem] h-[2.4rem] rounded-full" alt={'Nigdit icon'} loader={({src}) => src}/>
-                </div>
-                <div className='w-[5rem] hidden ml:block'>
-                    <p className="select-none shrink-1 text-[24px] font-['Roboto'] dark:text-white pl-2"><Link href={'/'}>NigDIT</Link></p>
-                </div>
-                <div className='w-[5.6rem] overflow-hidden shrink-1 hidden tl:flex'></div>
-            </Link>
+            {/* dashboard icon */}
+            <div className='flex flex-row'>
+                <Link href="/" className='pointer-events-auto min-w-[2.4rem] w-[2.4rem] ml:w-[7.4rem] tl:max-w-[13rem] h-[100%] flex flex-row my-2 mx-2'>
+                    <div className='shrink-0'>
+                        <Image draggable="false" src={nigditIcon} width={36} height={36} className="select-none hover:cursor-pointer object-cover overflow-hidden p-0 w-[2.4rem] h-[2.4rem] rounded-full" alt={'Nigdit icon'} loader={({src}) => src}/>
+                    </div>
+                    <div className='w-[5rem] hidden ml:block'>
+                        <p className="select-none shrink-1 text-[24px] font-['Roboto'] dark:text-white pl-2"><Link href={'/'}>NigDIT</Link></p>
+                    </div>
+                </Link>
+                <div className='max-w-[7.2rem] hidden tm:flex flex-row-reverse th:w-[7.2rem] tl:w-[4.8rem] w-[2.4rem]'></div>
+            </div>
+            {/* searchbar */}
             <div className='min-w-[12rem] w-[25vw] max-w-[30rem] h-[100%] grow flex flex-row my-2 mx-2'>
                 {
                     (searchValue != "")
@@ -80,7 +85,6 @@ export default function Navbar()
                             <div className='w-[100%] m-[0.2rem]'>
                                 <input value={searchValue} className='pointer-events-auto dark:text-white text-[1.2rem] bg-[rgba(0,0,0,0)] dark:bg-[rgba(0,0,0,0)] border-none outline-none w-[100%]' type="text" placeholder={"search..."} onChange={event => debouncedChangeHandler(event.target.value)} />
                             </div>
-                            {/* temp  */}
                             <button onClick={() =>{setSearchValue("")}} className='w-[2.1rem] min-w-[2.1rem] mr-1 p-[6px] hover:cursor-pointer pointer-events-auto'>
                                 <Image draggable="false" src={'/searchx.png'} width={33} height={33} className="" alt={''}/>
                             </button>
@@ -96,31 +100,61 @@ export default function Navbar()
                         </div>
                     )
                 }
-
             </div>
-            {(!!username) ? (
-            <Link href="/my-account" className='hover:cursor-pointer min-w-[2.4rem] tm:min-w-[13rem] h-[100%] flex flex-row-reverse my-2 ml-1 mr-3'>
-                <div className='w-[2.4rem] shrink-0'>
-                    <Image draggable="false" src={profilePicture ? (process.env.NEXT_PUBLIC_STRAPI_URL + profilePicture.url) : emptypfp.src} width={36} height={36} className="w-[2.4rem] pointer-events-auto select-none hover:cursor-pointer object-cover overflow-hidden rounded-full" alt={'Your profile picture'} loader={({src}) => src}/>
+            {/* user account panel */}
+            <div className='flex flex-row-reverse'>
+                {/* account info */}
+                <div className='ts:block hidden h-full'>
+                    {/* login buttons or user icon */}
+                    {(!!username) ? (
+                    <Link href="/my-account" className='hover:cursor-pointer w-[2.4rem] h-[100%] flex flex-row-reverse my-2 ml-1 mr-3'>
+                        <div className='w-[2.4rem] shrink-0'>
+                            <Image draggable="false" src={profilePicture ? (process.env.NEXT_PUBLIC_STRAPI_URL + profilePicture.url) : emptypfp.src} width={36} height={36} className="w-[2.4rem] pointer-events-auto select-none hover:cursor-pointer object-cover overflow-hidden rounded-full" alt={'Your profile picture'} loader={({src}) => src}/>
+                        </div>
+                        <div className='select-none overflow-hidden ml-auto shrink-1 hidden tm:block'>
+                            <p className="pointer-events-auto overflow-hidden text-right text-[20px] font-thin dark:text-white pr-2">{username}</p>
+                        </div>
+                    </Link>
+                    ):(
+                    <div className='min-w-[2.4rem] flex flex-row-reverse my-[6.5px] ml-1 mr-3'>
+                        <Link href="/register" className="pointer-events-auto hover:cursor-pointer mx-1 shrink-0 px-4 py-1 rounded-[666px] bg-[#aaa] text-[#373737] border-[1px] font-bold shadow-md border-[#000000] hover:bg-gray-100 transition-colors duration-300">
+                            register
+                        </Link>
+                        <Link href={"/login?redirect=" + router.asPath} className="pointer-events-auto hover:cursor-pointer shrink-0 px-4 py-1 rounded-[666px] bg-[#aaa] text-[#373737] border-[1px] font-bold shadow-md border-[#000000] hover:bg-gray-200 transition-colors duration-300">
+                            login
+                        </Link>
+                    </div>
+                    )}
                 </div>
-                <div className='select-none overflow-hidden ml-auto shrink-1 hidden tm:block'>
-                    <p className="pointer-events-auto overflow-hidden text-right text-[20px] font-thin dark:text-white pr-2">{username}</p>
+                {/* burger */}
+                <div className='ts:hidden h-full pointer-events-auto min-w-[2.4rem] w-[2.4rem] ml:w-[3.8rem] flex flex-row-reverse my-2 mx-2'>
+                    <div className='shrink-0'>
+                        {/* edytuj to */}
+                        <RxHamburgerMenu size={32} className="select-none hover:cursor-pointer object-cover overflow-hidden p-0 w-[2.4rem] h-[2.4rem]"/>
+                    </div>
                 </div>
-            </Link>
-            ):(
-            <div className='min-w-[2.4rem] tm:min-w-[13rem] flex flex-row-reverse my-[6.5px] ml-1 mr-3'>
-                <Link href="/register" className="pointer-events-auto hover:cursor-pointer mx-1 shrink-0 px-4 py-1 rounded-[666px] bg-[#aaa] text-[#373737] border-[1px] font-bold shadow-md border-[#000000] hover:bg-gray-100 transition-colors duration-300">
-                    register
-                </Link>
-                <Link href={"/login?redirect=" + router.asPath} className="pointer-events-auto hover:cursor-pointer shrink-0 px-4 py-1 rounded-[666px] bg-[#aaa] text-[#373737] border-[1px] font-bold shadow-md border-[#000000] hover:bg-gray-200 transition-colors duration-300">
-                    login
-                </Link>
+                {/* buttons */}
+                <div className='max-w-[7.2rem] hidden tm:flex flex-row-reverse'>
+                    <div className='w-[2.4rem] tl:flex hidden items-center h-full p-1 '>
+                        {/* dostylizuj to olo */}
+                        <BiMessageAdd size={32} className="select-none hover:cursor-pointer object-cover overflow-hidden w-full h-full"/>
+                    </div>
+                    <div className='w-[2.4rem] items-center h-full hidden tm:flex '>
+                        {/* icon 2 - empty disappears at the latest*/}
+                    </div>
+                    <div className='w-[2.4rem] items-center h-full hidden th:flex '>
+                        {/* icon 3 - disappears at the earliest*/}
+                    </div>
+                </div>
             </div>
-            )}
+            {/* search results FRAGILE*/}
             {
                 (searchValue != "") ? (
                     <div className='overflow-hidden flex flex-row fixed w-[100%] justify-between left-0 mt-[25px] '>
-                    <div className='min-w-[2.4rem] w-[2.4rem] ml:w-[7.4rem] tl:max-w-[13rem] h-[100%] flex flex-row my-2 mx-2'></div>
+                    <div className='flex flex-row'>
+                        <div className='min-w-[2.4rem] w-[2.4rem] ml:w-[7.4rem] tl:max-w-[13rem] h-[100%] flex flex-row my-2 mx-2'></div>
+                        <div className='max-w-[7.2rem] hidden tm:flex flex-row-reverse th:w-[7.2rem] tl:w-[4.8rem] w-[2.4rem]'></div>
+                    </div>
                     <div className='min-w-[12rem] w-[25vw] max-w-[30rem] h-[100%] grow flex flex-col my-2 mx-2'>
                         <div className='w-full h-[20px] bg-backgroundD border-accentD border-x-[1px]'></div>
                         {
@@ -140,24 +174,29 @@ export default function Navbar()
                         }
                         <div className='w-full h-[10px] bg-backgroundD rounded-b-[10px] border-accentD border-[1px] border-t-[0px]'></div>
                     </div>
-                    {(isLogged) ? (
-                    <div className='hover:cursor-pointer min-w-[2.4rem] tm:min-w-[13rem] h-[100%] flex flex-row-reverse my-2 ml-1 mr-3'>
-                        <div className='w-[2.4rem] shrink-0'></div>
-                        <div className='shrink-1 hidden tm:block'>
-                            <p className=" text-[20px] text-transparent font-thin pr-2">{username}</p>
+                    <div className='flex flex-row-reverse'>
+                        <div className='ts:block hidden'>
+                            {(isLogged) ? (
+                            <div className='hover:cursor-pointer min-w-[2.4rem] h-[100%] flex flex-row-reverse my-2 ml-1 mr-3'>
+                                <div className='w-[2.4rem] shrink-0'></div>
+                                <div className='shrink-1 hidden tm:block'>
+                                    <p className=" text-[20px] text-transparent font-thin pr-2">{username}</p>
+                                </div>
+                            </div>
+                            ):(
+                            <div className='min-w-[2.4rem] flex flex-row-reverse my-[6.5px] ml-1 mr-3'>
+                                <div className='w-[172px]'></div>
+                            </div>
+                            )}
                         </div>
+                        <div className='ts:hidden h-full pointer-events-auto w-[2.4rem] ml:w-[3.8rem] flex flex-row-reverse my-2 mx-2'></div>
+                        <div className='max-w-[7.2rem] hidden tm:flex flex-row-reverse th:w-[7.2rem] tl:w-[4.8rem] w-[2.4rem]'></div>
                     </div>
-                    ):(
-                    <div className='min-w-[2.4rem] tm:min-w-[13rem] flex flex-row-reverse my-[6.5px] ml-1 mr-3'>
-                        <div className='w-[172px]'></div>
-                    </div>
-                    )}
                 </div>
                 ):(
                     ""
                 )
             }
-
         </div>
     );
 }
