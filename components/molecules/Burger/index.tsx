daeaa055ed898886efdbe6d3e8cca2188d7c4af9
Oliwier Kwiatkowski <oliwierkwiatkowski@protonmail.com>
2023-11-25 17:36:53 +0100

    Change <a> to <Link> and <p>

--- components/molecules/Burger/index.tsx
+++ components/molecules/Burger/index.tsx
@@ -1,28 +1,31 @@
-import { slide as Menu } from 'react-burger-menu'
+import Link from 'next/link';
+import { slide as Menu } from 'react-burger-menu';
 
 type BurgerProps = {
-    burgerOpen: boolean;
-    setBurgerOpen: (burgerOpen: boolean) => void;
-    dictionary: {
-        [key: string]: string;
-    };
-    
-}
+  burgerOpen: boolean;
+  setBurgerOpen: (burgerOpen: boolean) => void;
+  dictionary: {
+    [key: string]: string;
+  };
+};
 
-export default function Burger({ burgerOpen, setBurgerOpen, dictionary }: BurgerProps) {
-
-    return(
-        <div>
-            <Menu right>
-                {Object.entries(dictionary).map(([key, value]) => (
-                    <div key={key}>
-                        <a className="menu-item" href={`${value}`}>
-                            {key}
-                        </a>
-                        <a onClick={() => setBurgerOpen(false)}>Close</a>
-                    </div>
-                ))}
-            </Menu>
-        </div>
-    )
+export default function Burger({
+  burgerOpen,
+  setBurgerOpen,
+  dictionary,
+}: BurgerProps) {
+  return (
+    <div>
+      <Menu right>
+        {Object.entries(dictionary).map(([key, value]) => (
+          <div key={key}>
+            <Link className="menu-item" href={`${value}`}>
+              {key}
+            </Link>
+            <p onClick={() => setBurgerOpen(false)}>Close</p>
+          </div>
+        ))}
+      </Menu>
+    </div>
+  );
 }
