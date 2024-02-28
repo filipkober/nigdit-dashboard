6311e869072bd39f13881336a78468f9aa8a9255
Oliwier Kwiatkowski <oliwierkwiatkowski@protonmail.com>
2024-02-28 23:46:09 +0100

    Fix Join/Edit button depending on isOwner In Progress - Fix Member count on SubnigditInfo

--- components/atoms/JoinButton/index.tsx
+++ components/atoms/JoinButton/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { GenericComponentProps } from '../../../models/GenericComponentProps';
 import { StrapiSubnigdit } from '../../../models/Subnigdit';
@@ -22,21 +23,71 @@ export function JoinButton({ className, subnigdit, onJoin }: JoinButtonProps) {
   function joinThisSub() {
     onJoin?.(!joined);
     setJoined(!joined);
-    subnigditService.joinSubnigdit(subnigdit.id.toString()).then((response) => {
-      console.log('has user joined: ' + response);
-    });
+    subnigditService.joinSubnigdit(subnigdit.id.toString());
   }
 
-  return (
-    <>
-      <button
-        onClick={() => {
-          joinThisSub();
-        }}
-        className="w-[8rem] justify-center py-3 font-semibold text-lg text-white rounded-full shadow-sm border-4 border-white"
-      >
-        {!joined ? 'Join' : 'Joined'}
-      </button>
-    </>
-  );
+  const router = useRouter();
+  const name = subnigdit?.attributes?.name_uid;
+
+  const [isOwner, setIsOwner] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (name) {
+      subnigditService.getBySlug(name as string, true).then((res) => {
+        if (res[0]?.attributes?.owner?.data?.id == user?.id) {
+          setIsOwner(true);
+        } else {
+          setIsOwner(false);
+        }
+      });
+    }
+  }, [subnigdit?.attributes?.name_uid]);
+
+  function userType() {
+    if (isOwner) {
+      return 'owner';
+    } else if (isLogged) {
+      return 'user';
+    } else {
+      return 'guest';
+    }
+  }
+
+  const isLogged = !!useSelector((state: UserState) => state.user.username);
+
+  switch (userType()) {
+    case 'owner':
+      return (
+        <button
+          onClick={() => {
+            router.push(`/n/${subnigdit.attributes.name_uid}/edit`);
+          }}
+          className="w-[8rem] justify-center py-3 font-semibold text-lg text-white rounded-full shadow-sm border-4 border-white"
+        >
+          Edit
+        </button>
+      );
+    case 'user':
+      return (
+        <button
+          onClick={() => {
+            joinThisSub();
+          }}
+          className="w-[8rem] justify-center py-3 font-semibold text-lg text-white rounded-full shadow-sm border-4 border-white"
+        >
+          {!joined ? 'Join' : 'Joined'}
+        </button>
+      );
+    case 'guest':
+      return (
+        <button
+          onClick={() => {
+            router.push('/login');
+          }}
+          className="w-[8rem] justify-center py-3 font-semibold text-lg text-white rounded-full shadow-sm border-4 border-white"
+        >
+          Join
+        </button>
+      );
+  }
 }
