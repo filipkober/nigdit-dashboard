b79d8d423c1f7f176bf057c87426c356b1f13890
Damian Jedrzejewski <Damian035jedrzejewski@gmail.com>
2024-04-03 23:25:06 +0200

    tiny fix

--- components/atoms/CreateSubButton/index.tsx
+++ components/atoms/CreateSubButton/index.tsx
@@ -5,7 +5,7 @@ export default function CreateSubButton() {
     <>
       <button
         onClick={() => router.push('/new/subnigdit')}
-        className="bg-green-700 hover:bg-green-600 text-white font-bold py-0 px-4 border-b-4 border-green-900 hover:border-green-700 rounded-[10px]"
+        className="min-h-[40px] bg-green-700 hover:bg-green-600 text-white font-bold py-0 px-4 border-b-4 border-green-900 hover:border-green-700 rounded-[10px]"
       >
         Create your own community now!
       </button>
--- components/organism/DashboardFeed/index.tsx
+++ components/organism/DashboardFeed/index.tsx
@@ -182,7 +182,7 @@ export default function DashboardFeed() {
             </div>
           )}
           {!isOwner && isLogged && (
-            <div className="w-[100%] ls:w-[80%] flex flex-row justify-center tl:pt-2 p-0 m-0">
+            <div className="w-[100%] ls:w-[80%] flex flex-row justify-center p-2 m-0">
               <CreateSubButton/>
             </div>
           )}
